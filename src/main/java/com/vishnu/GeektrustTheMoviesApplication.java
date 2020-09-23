54dd3e9bdae22de9b73014552cc4fe792e986d39
Vishnu Gupta <vishnugupta@fico.com>
2020-09-23 17:38:41 +0530

    Initial Commit

---
+++ src/main/java/com/vishnu/GeektrustTheMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.vishnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeektrustTheMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeektrustTheMoviesApplication.class, args);
+	}
+
+}
